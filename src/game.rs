2b8f8c4b627df69347dec9c6ecded3640232fbf2
Stuart Haidon <serayen.sh@gmail.com>
2019-07-09 14:59:41 +0100

    Converted try_ and move_ flags to enum.

--- src/game.rs
+++ src/game.rs
@@ -45,13 +45,9 @@ impl Game {
 
                 on_ground: true,
 
-                try_right: false,
-                try_left: false,
-                try_jump: false,
-
-                do_right: false,
-                do_left: false,
-                do_jump: false,
+                right: MoveState::None,
+                left: MoveState::None,
+                jump: MoveState::None,
                 jump_timer: 0,
 
                 collision_point: [false; 8],
@@ -96,13 +92,20 @@ impl Game {
         self.dave.move_dave();
         self.scroll_screen();
         self.dave.apply_gravity(self.current_level, assets);
-        self.dave.clear_input();
     }
 
     pub fn input(&mut self, input: &Input) {
-        self.dave.try_right = input.right();
-        self.dave.try_left = input.left();
-        self.dave.try_jump = input.jump();
+        if input.right() {
+            self.dave.right = MoveState::Try;
+        }
+
+        if input.left() {
+            self.dave.left = MoveState::Try;
+        }
+
+        if self.dave.jump == MoveState::None && input.jump() {
+            self.dave.jump = MoveState::Try;
+        }
     }
 }
 
@@ -111,13 +114,9 @@ pub struct Dave {
     pub position: Position<u8>,
     pub pixel_position: Position<i16>,
 
-    try_right: bool,
-    try_left: bool,
-    try_jump: bool,
-
-    do_right: bool,
-    do_left: bool,
-    do_jump: bool,
+    right: MoveState,
+    left: MoveState,
+    jump: MoveState,
     jump_timer: u8,
 
     on_ground: bool,
@@ -128,7 +127,7 @@ pub struct Dave {
 
 impl Dave {
     fn apply_gravity(&mut self, level: LevelId, assets: &Assets) {
-        if !self.do_jump && !self.on_ground {
+        if self.jump != MoveState::Do && !self.on_ground {
             let is_clear = (self.is_clear(level, assets, self.pixel_position + (4, 17)),
                 self.is_clear(level, assets, self.pixel_position + (10,17)));
 
@@ -150,17 +149,19 @@ impl Dave {
     }
 
     fn move_dave(&mut self) {
-        if self.do_right {
+        use MoveState::*;
+
+        if self.right == Do {
             self.pixel_position.x += 2;
-            self.do_right = false;
+            self.right = None;
         }
 
-        if self.do_left {
+        if self.left == Do {
             self.pixel_position.x -= 2;
-            self.do_left = false;
+            self.left = None;
         }
 
-        if self.do_jump {
+        if self.jump == Do {
             if self.jump_timer == 0 {
                 self.jump_timer = 20;
             }
@@ -178,7 +179,7 @@ impl Dave {
             }
 
             if self.jump_timer == 0 {
-                self.do_jump = false;
+                self.jump = None;
             }
         }
     }
@@ -229,24 +230,27 @@ impl Dave {
     }
 
     fn verify_input(&mut self) {
-        if self.try_left && self.collision_point[6] && self.collision_point[7] {
-            self.do_left = true;
+        use MoveState::*;
+
+        if self.left == Try && self.collision_point[6] && self.collision_point[7] {
+            self.left = Do;
         }
 
-        if self.try_right && self.collision_point[2] && self.collision_point[3] {
-            self.do_right = true;
+        if self.right == Try && self.collision_point[2] && self.collision_point[3] {
+            self.right = Do;
         }
 
-        if self.try_jump && !self.do_jump && self.on_ground && self.collision_point[0] && self.collision_point[1] {
-            self.do_jump = true;
+        if self.jump == Try && self.on_ground && self.collision_point[0] && self.collision_point[1] {
+            self.jump = Do;
         }
     }
+}
 
-    fn clear_input(&mut self) {
-        self.try_jump = false;
-        self.try_right = false;
-        self.try_left = false;
-    }
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum MoveState {
+    None,
+    Try,
+    Do,
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
